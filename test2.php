7e15e5788e45febfef8ddbcc809c056679781e09
everythingsite <likeanavanish@gmail.com>
2016-06-06 13:44:10 +0000

    hi

--- test2.php
+++ test2.php
@@ -1,5 +1,5 @@
 <?php
-if($_POST["message"]) {
-    mail("akarlapudi03@gmail.com", "everthing-site registration", $_POST["message"], "From: hi@gmail.com");
+if($_POST["post"]) {
+    mail("akarlapudi03@gmail.com", "everthing-site registration", $_POST["post"], "From: hi@gmail.com");
 }
 ?>
