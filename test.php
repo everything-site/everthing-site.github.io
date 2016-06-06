a1c408c41b29a69ab9052c23ebbca3efab43b979
everythingsite <likeanavanish@gmail.com>
2016-06-06 12:02:46 +0000

    hi

--- test.php
+++ test.php
@@ -3,6 +3,7 @@ if($_POST["message"]) {
     mail("akarlapudi03@gmail.com", "everthing-site registration", $_POST["message"], "From: bot@everything-site.github.io");
 }
 ?>
+
 <!DOCTYPE html>
 
 <form method="post" action="test.html">
