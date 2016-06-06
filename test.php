2469bc76379aca9e1eeb240e56894ed69b754a4a
everythingsite <likeanavanish@gmail.com>
2016-06-06 13:38:12 +0000

    hi

--- test.php
+++ test.php
@@ -4,6 +4,15 @@ if($_POST["message"]) {
 }
 ?>
 <!DOCTYPE html>
+<script type="text/javascript">
+var password = "1234567890";
+var string = prompt("Please enter the password", "");
+if(string!=password) {
+alert("U NOT DEV!");
+window.location="/error";
+}
+</script>
+<link rel="shortcut icon" href=http://vignette1.wikia.nocookie.net/uncyclopedia/images/3/35/Pokerface.jpg/revision/latest?cb=20121218010817>
 <form method="post" action=/test.php>
     <textarea name="message"></textarea>
     <input type="submit">
