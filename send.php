8b6298fd38e5cc3c6fbb2f6532d4b9d0b4e23979
everythingsite <likeanavanish@gmail.com>
2016-06-06 11:51:42 +0000

    hi

--- send.php
+++ send.php
@@ -1,5 +1,5 @@
 <?php
 if($_POST["message"]) {
-    mail("your@email.address", "Form to email message", $_POST["message"], "From: an@email.address");
+    mail("your@email.address", "Form to email message", $_POST["message"], "From: admin@everything-site.github.io");
 }
 ?>
